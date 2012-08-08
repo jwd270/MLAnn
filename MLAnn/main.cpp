c5b826658eb45c4531dac964d8fb74792fd64ee0
Jim <jim@j2ntech.com>
2012-08-08 08:06:07 -0400

    Initial Commit

---
+++ MLAnn/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MLAnn
+//
+//  Created by James Dean on 8/8/12.
+//  Copyright (c) 2012 James Dean. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
